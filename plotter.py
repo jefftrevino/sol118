7c647996bdc889636cddf62b9c3f8b4721c92f9f
Jeffrey Trevino <jeffrey.trevino@gmail.com>
2017-01-07 22:48:33 -0800

    revised according to new paper-aware plotter definition

---
+++ plotter.py
@@ -0,0 +1,204 @@
+import svgwrite
+import uuid
+from shapely.geometry import Polygon, Point, box
+from shapely.affinity import scale, translate
+from chiplotle import (
+    hpgl,
+    instantiate_plotters
+    )
+
+
+def position_and_size_of_geom(geom):
+    """Returns xmin, ymin, width, and height of a geometry
+    based on its shapely `.bounds`
+    """
+    bounds = geom.bounds
+    width = bounds[2] - bounds[0]
+    height = bounds[3] - bounds[1]
+    return bounds[0], bounds[1], width, height
+
+
+def px(*args):
+    return [str(item) + "px" for item in args]
+
+
+PLOTTER_UNITS_PER_INCH = 1018.39880656
+
+
+class Drawing:
+    """Assumes that everything is in inches
+
+    Before plotting or making previews, all geometry is
+    translated into plotter units.
+    """
+
+    def __init__(self, default_scale=PLOTTER_UNITS_PER_INCH):
+        self.geoms = []
+        self.get_bounds()
+        self.default_preview_filepath = "previews/preview.svg"
+        self.plotter = None
+        self.scalar = default_scale
+        self.paper = None
+        self.paper_origin = Point(-11.7850105901, -8.765625)
+        self.add_paper(5, 3)
+
+    def get_bounds(self):
+        self.bounds_poly = Polygon([
+            (-11640, -8640),
+            (-11640, 8640),
+            (10720, 8640),
+            (10720, -8640),
+            (-11640, -8640),
+            ])
+        self.bounds = self.bounds_poly.bounds
+        self.width = 11640 + 10720
+        self.height = 8640 * 2
+
+    def plot(self):
+        if not self.plotter:
+            plotters = instantiate_plotters()
+            self.plotter = plotters[0]
+        for geom in self.geoms:
+            self.plot_geom(geom)
+
+    def add(self, geom):
+        self.geoms.append(self.scale_to_plotter_units(geom))
+
+    def add_paper(self, width, height):
+        """paper width and height are assumed to be in scalar units
+            (inches by default)
+        """
+        geom = box(
+            minx=0, miny=0,
+            maxx=width, maxy=height
+            )
+        self.paper = translate(
+            geom,
+            xoff=self.paper_origin.x,
+            yoff=self.paper_origin.y
+            )
+        self.paper_center = self.paper.centroid
+        return self.paper
+
+    def scale_to_plotter_units(self, geom):
+        return scale(geom, self.scalar, self.scalar, origin=Point(0, 0))
+
+    def clip_to_plotter_bounds(self):
+        """Clips all geometries to the boundaries of the plotter
+        """
+        self.geoms = [
+            self.bounds_poly.intersection(geom)
+            for geom in self.geoms
+        ]
+
+    def plot_geom(self, geom):
+        if hasattr(geom, 'coords'):
+            # assume it is a linear ring or linestring
+            self.plot_coords([coord for coord in geom.coords])
+        elif hasattr(geom, 'exterior'):
+            # assume it has a Polygon-like interface
+            self.plot_geom(geom.exterior)
+            for ring in geom.interiors:
+                self.plot_geom(ring)
+        elif hasattr(geom, 'geoms'):
+            # assume this is a collection of objects
+            for geom in geom.geoms:
+                self.plot_geom(geom)
+        else:
+            raise NotImplementedError(
+                "I don't know how to plot {}".format(type(geom)))
+
+    def start_svg(self):
+        preview_margin = 100
+        screen_height = 600
+        plotter_paper = self.scale_to_plotter_units(self.paper)
+        paper_x, paper_y, paper_width, paper_height = \
+            position_and_size_of_geom(plotter_paper)
+        paper_top = paper_y + paper_height
+        svg_width = paper_width + (preview_margin * 2)
+        svg_height = paper_height + (preview_margin * 2)
+        screen_width = (svg_width / float(svg_height)) * screen_height
+        self.svg = svgwrite.Drawing(
+            filename=self.default_preview_filepath,
+            size=px(screen_width, screen_height),
+            style="background-color: #ccc"
+            )
+        self.svg.viewbox(
+            minx=paper_x - preview_margin,
+            miny=(paper_top * -1) - preview_margin,
+            width=svg_width,
+            height=svg_height,
+            )
+        self.plotter_geom_group = self.svg.g(
+            transform="scale(1, -1)"
+            )
+        # draw paper
+        self.plotter_geom_group.add(self.svg.rect(
+            insert=(paper_x, paper_y),
+            size=(paper_width, paper_height),
+            fill="white",
+            ))
+
+    def preview(self, filepath=None):
+        self.start_svg()
+        for geom in self.geoms:
+            self.preview_geom(geom)
+        self.svg.add(self.plotter_geom_group)
+        self.add_bounds_preview()
+        if not filepath:
+            self.svg.save()
+        else:
+            self.svg.saveas(filepath)
+
+    def preview_geom(self, geom, **kwargs):
+        if hasattr(geom, 'xy'):
+            # assume it is a linear ring or linestring
+            self.plotter_geom_group.add(self.svg.polyline(
+                points=geom.coords,
+                stroke_width="1",
+                fill="none",
+                stroke="black",)
+            )
+        elif hasattr(geom, 'exterior'):
+            # assume it has a Polygon-like interface
+            self.preview_geom(geom.exterior, **kwargs)
+            for ring in geom.interiors:
+                self.preview_geom(ring, **kwargs)
+        elif hasattr(geom, 'geoms'):
+            # assume this is a collection of objects
+            for geom in geom.geoms:
+                self.preview_geom(geom, **kwargs)
+        else:
+            raise NotImplementedError(
+                "I don't know how to preview {}".format(type(geom)))
+
+    def add_bounds_preview(self):
+        self.svg.add(self.svg.rect(
+            insert=(self.bounds[0], self.bounds[1]),
+            size=(self.width, self.height),
+            stroke_width=25,
+            stroke_dasharray=100,
+            stroke="black",
+            fill="none"
+            ))
+
+    def plot_coords(self, coords):
+        start = hpgl.PU([coords[0]])
+        self.plotter.write(start)
+        threshold = 300
+        while len(coords) > threshold:
+            command = hpgl.PD(coords[:threshold])
+            self.plotter.write(command)
+            coords = coords[threshold:]
+        command = hpgl.PD(coords)
+        self.plotter.write(command)
+        end = hpgl.PU([coords[-1]])
+        self.plotter.write(end)
+
+    def scale_to_fit(self, geom):
+        return scale(
+            geom,
+            xfact=self.scale_ratio,
+            yfact=self.scale_ratio,
+            origin=(0.0, 0.0),
+            )
--- sol118.py
+++ sol118.py
@@ -1,92 +1,30 @@
-from plotterWsvgPreview import Drawing
+from plotter import Drawing
 from shapely.geometry import LineString, Point, GeometryCollection
 from shapely.affinity import scale, rotate, translate
 from shapely.ops import cascaded_union
-from random import randint, seed as set_seed
+from random import uniform, seed as set_seed
 from math import floor
-
-set_seed(2)
 
 
 def points_to_coord_tuples(points):
     return [(p.x, p.y) for p in points]
 
 
-def make_hexagon(center, radius):
-    # center is a Shapely point
-    coords = [Point(center.x + radius, center.y)]
-    point = coords[0]
-    for x in range(6):
-        new_point = rotate(point, 60 * x, origin=center)
-        coords.append(new_point)
-    coord_tuples = points_to_coord_tuples(coords)
-    hexagon = Polygon(coord_tuples)
-    return hexagon
-
-
-def make_hexagons(num_hexagons, xmin, xmax, ymin, ymax, rmin, rmax):
-    hexagons = []
-    for x in range(num_hexagons):
-        center_x = randint(xmin, xmax)
-        center_y = randint(ymin, ymax)
-        radius = randint(rmin, rmax)
-        hexagons.append(
-            make_hexagon(
-                Point(center_x, center_y),
-                radius
-            )
-        )
-    return hexagons
-
-
-def make_shank(hexagons):
-    union = cascaded_union(hexagons)
-    union_reflection = scale(union, -1.0, 1.0, origin=Point(0, 0))
-    return cascaded_union([union, union_reflection])
-
-
-def twist_shank(shank, center):
-    shankles = [shank]
-    for x in range(1, 6):
-        shanklet = rotate(shank, 60 * x, origin=center)
-        shankles.append(shanklet)
-    return cascaded_union(shankles)
-
-
-def make_snowflake_layer(num_hexagons, r_min, r_max):
-    hexagons = make_hexagons(num_hexagons, 0, 35, 0, 500, r_min, r_max)
-    shank = make_shank(hexagons)
-    full_snowflake = twist_shank(shank, Point(0, 0))
-    return full_snowflake
-
-
-def make_snowflake(seed, origin, rotation=0):
-    """origin is assumed to be a shapely.geometry.Point
-    rotation is in degrees
-    """
-    set_seed(seed)
-    base_layer = make_snowflake_layer(5, 50, 150)
-    top_layer = make_snowflake_layer(20, 5, 50)
-    snowflake = GeometryCollection([base_layer, top_layer])
-    return rotate(
-        translate(snowflake, xoff=origin.x, yoff=origin.y),
-        rotation, origin=origin)
-
-
-def run():
+def run(seed_int):
+    set_seed(seed_int)
     drawing = Drawing()
-    x_offset = -10000
-    y_offset = -8000
-    horizontal_margin = 1200
-    vertical_margin = 1400
+    x_offset = -11.5
+    y_offset = -8.5
+    horizontal_margin = .5
+    vertical_margin = .5
     point_x_min = horizontal_margin
-    point_x_max = drawing.width-horizontal_margin
+    point_x_max = 5-horizontal_margin
     point_y_min = vertical_margin
-    point_y_max = drawing.height-vertical_margin
+    point_y_max = 3-vertical_margin
     points = [
         Point(
-            randint(point_x_min, point_x_max) + x_offset,
-            randint(point_y_min, point_y_max) + y_offset)
+            uniform(point_x_min, point_x_max) + x_offset,
+            uniform(point_y_min, point_y_max) + y_offset)
         for x in range(50)
         ]
     coord_tuples = points_to_coord_tuples(points)
@@ -96,9 +34,11 @@ def run():
                 line = LineString([coord_tuple, coord_tuples[i]])
                 drawing.add(line)
     # line = LineString([(0, 0), (drawing.width, drawing.height)])
-    # drawing.add(line)
-    drawing.preview()
+    drawing.add(line)
+    drawing.preview(filepath='previews/preview-seed-' + str(seed_int) + '.svg')
     # drawing.plot()
+    return drawing
 
 if __name__ == '__main__':
-    run()
+    for seed_int in range(20, 40):
+        drawing = run(seed_int)
